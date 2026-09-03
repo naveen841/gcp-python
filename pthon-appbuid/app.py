f1600335e9d6215ea564d628b230eba2bb56d089
naveen841 <79522681+naveen841@users.noreply.github.com>
2026-09-03 17:08:35 +0530

    Update app.py

--- pthon-appbuid/app.py
+++ pthon-appbuid/app.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 
 def hello_world():
 
-    return "Hello, World! This is my first Python application deployed on GCP using Cloud Build and Cloud Run. I am excited to learn more about GCP and Python and build more applications in the future."
+    return "Hello, World! This is my first Python application deployed on GCP using Cloud Build and Cloud Run. I am excited to learn more about GCP and Python and build more applications in the future it's successful running."
  
 @app.route("/welcome")
 
